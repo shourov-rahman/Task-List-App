37cc200fcb53dd9bff028926a570074ff443f60f
Shourov <shourovju43@gmail.com>
2021-08-02 15:53:05 +0600

    Remove and clear task from local storage

--- app.js
+++ app.js
@@ -125,7 +125,28 @@ function storeTaskInLocalStorage(task){
 function removeTask(e){
   if(e.target.parentElement.classList.contains('delete-item')){
     e.target.parentElement.parentElement.remove();
+
+    //Remove from Local Storage
+    //Note (e.target.parentElement.parentElement= li)
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement);
   }
+}
+
+//Remove from Local Storage
+function removeTaskFromLocalStorage(taskItem){
+  let tasks;
+  if(localStorage.getItem('tasks')===null){
+    tasks = [];
+  }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task,index){
+    if(taskItem.textContent===task){
+      tasks.splice(index,1);
+    }
+  });
+  localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 
 //Clear Task function
@@ -145,6 +166,13 @@ function clearTasks(){
   while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
+
+  // Clear Task from local storage
+  clearTasksFromLocalStorage();
+}
+//Clear Task from local storage
+function clearTasksFromLocalStorage(){
+  localStorage.clear();
 }
 
 //Filter Tasks
